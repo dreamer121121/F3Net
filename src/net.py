b3c822691b92898b319f2e3c5c82c5eda4e6de54
夏涛 <xt5@meitu.com>
2021-03-31 17:47:43 +0800

    add SE to backbone

--- src/net.py
+++ src/net.py
@@ -7,6 +7,65 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+# class SEBottleneck(nn.Module):
+#     expansion = 4
+#
+#     def __init__(self, inplanes, planes, stride=1, downsample=None, reduction=16):
+#         super(SEBottleneck, self).__init__()
+#         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+#         self.bn1 = nn.BatchNorm2d(planes)
+#         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+#                                padding=1, bias=False)
+#         self.bn2 = nn.BatchNorm2d(planes)
+#         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+#         self.bn3 = nn.BatchNorm2d(planes * 4)
+#         self.relu = nn.ReLU(inplace=True)
+#         self.se = SELayer(planes * 4, reduction)
+#         self.downsample = downsample
+#         self.stride = stride
+#
+#     def forward(self, x):
+#         residual = x
+#
+#         out = self.conv1(x)
+#         out = self.bn1(out)
+#         out = self.relu(out)
+#
+#         out = self.conv2(out)
+#         out = self.bn2(out)
+#         out = self.relu(out)
+#
+#         out = self.conv3(out)
+#         out = self.bn3(out)
+#         out = self.se(out)
+#
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
+#
+#         out += residual
+#         out = self.relu(out)
+#
+#         return out
+
+
 def weight_init(module):
     for n, m in module.named_children():
         print('initialize: '+n)
@@ -39,12 +98,14 @@ class Bottleneck(nn.Module):
         self.bn2        = nn.BatchNorm2d(planes)
         self.conv3      = nn.Conv2d(planes, planes*4, kernel_size=1, bias=False)
         self.bn3        = nn.BatchNorm2d(planes*4)
+        self.se = SELayer(planes * 4, reduction=16)
         self.downsample = downsample
 
     def forward(self, x):
         out = F.relu(self.bn1(self.conv1(x)), inplace=True)
         out = F.relu(self.bn2(self.conv2(out)), inplace=True)
         out = self.bn3(self.conv3(out))
+        out = self.se(out)
         if self.downsample is not None:
             x = self.downsample(x)
         return F.relu(out+x, inplace=True)
