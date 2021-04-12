8a632f37092e700332224055abb258551b3f7697
xiatao <2761564455@qq.com>
2021-04-12 18:03:51 +0800

    update epochs to 32

--- src/train.py
+++ src/train.py
@@ -67,7 +67,7 @@ def main(Dataset,Network):
     args = parse_args()
 
     train_cfg = Dataset.Config(datapath='../data/DUTS/', savepath='./out', snapshot=args.resume, mode='train', batch=32,
-                            lr=0.05, momen=0.9, decay=5e-4, epochs=100)
+                            lr=0.05, momen=0.9, decay=5e-4, epochs=32)
 
 
     eval_cfg =  Dataset.Config(datapath='../data/DUTS/', mode='test',eval_freq=1)
