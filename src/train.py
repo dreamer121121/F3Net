d096f598e15ff2fb84a1b247595595547d8d7a42
xiatao <2761564455@qq.com>
2021-04-13 10:02:35 +0800

    fix bug in structure loss

--- src/train.py
+++ src/train.py
@@ -70,7 +70,7 @@ def im2tensor(image_numpy):
 
 
 def structure_loss(pred, mask):
-    # weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
+    weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
     # wbce  = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
     # wbce  = (weit*wbce).sum(dim=(2,3))/weit.sum(dim=(2,3))
 
