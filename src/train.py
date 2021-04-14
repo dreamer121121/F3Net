2542411961c16f86ae83fc1c07186124f0c8e17f
xiatao <2761564455@qq.com>
2021-04-14 14:14:34 +0800

    add mixup

--- src/train.py
+++ src/train.py
@@ -61,6 +61,28 @@ def structure_loss(pred, mask):
     return (wbce+wiou).mean()
 
 
+def mixup_data(x, y, alpha=1.0, use_cuda=True):
+    '''Returns mixed inputs, pairs of targets, and lambda'''
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    if use_cuda:
+        index = torch.randperm(batch_size).cuda()
+    else:
+        index = torch.randperm(batch_size)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+
 def main(Dataset,Network):
 
     ##parse args
@@ -156,6 +178,8 @@ def train(net,optimizer,loader,sw,epoch,cfg):
     net.train()
     for step, (image, mask) in enumerate(loader):
         image, mask = image.cuda().float(), mask.cuda().float()
+
+        image,mask_a,mask_b = mixup_data(image,mask,use_cuda=)
         # print(image.shape) #(32,3,320,320)
         # print(mask.shape) #(32,1,320,320)
         # import sys
