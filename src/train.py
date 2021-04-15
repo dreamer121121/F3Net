2f9774a04533928a305abb685c415f85c8fee2f4
xiatao <2761564455@qq.com>
2021-04-15 13:36:00 +0800

    del wbce loss

--- src/train.py
+++ src/train.py
@@ -73,11 +73,11 @@ def structure_loss(pred, mask):
     weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
     # wbce  = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
     # wbce  = (weit*wbce).sum(dim=(2,3))/weit.sum(dim=(2,3))
-
-    pred  = torch.sigmoid(pred)
-    inter = ((pred*mask)*weit).sum(dim=(2,3))
-    union = ((pred+mask)*weit).sum(dim=(2,3))
-    wiou  = 1-(inter+1)/(union-inter+1)
+    #
+    # pred  = torch.sigmoid(pred)
+    # inter = ((pred*mask)*weit).sum(dim=(2,3))
+    # union = ((pred+mask)*weit).sum(dim=(2,3))
+    # wiou  = 1-(inter+1)/(union-inter+1)
 
     #add cortor loss
     N,C,W,H = mask.shape
@@ -110,7 +110,7 @@ def structure_loss(pred, mask):
     # sys.exit(0)
 
 
-    return (wiou+cortor_loss).mean()
+    return (cortor_loss).mean()
 
 
 def main(Dataset,Network):
