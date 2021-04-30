5c13fa8e973c79a0835ae73b34a7a01140af1fe9
xiatao <2761564455@qq.com>
2021-04-30 16:19:03 +0800

    fix bug in net

--- src/net.py
+++ src/net.py
@@ -193,8 +193,8 @@ class F3Net(nn.Module):
         if self.cfg.snapshot: #finetune
             if os.path.isfile(self.cfg.snapshot):
                 print('=> loading checkpoint {} \n'.format(self.cfg.snapshot))
-            #checkpoints = torch.load(self.cfg.snapshot)['state_dict']
-            checkpoints = torch.load(self.cfg.snapshot)
+            checkpoints = torch.load(self.cfg.snapshot)['state_dict']
+            #checkpoints = torch.load(self.cfg.snapshot)
             fielter_checkpoints = dict()
             for k,v in checkpoints.items():
                 if "module" in k:
@@ -205,7 +205,7 @@ class F3Net(nn.Module):
             #import sys
             #sys.exit(0)
             self.load_state_dict(fielter_checkpoints)
-            import sys
-            sys.exit(0)
+            #import sys
+            #sys.exit(0)
         else:
             weight_init(self)
