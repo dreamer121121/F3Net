d28f72aeeb78ad6948d2f074cce93dcd24892c09
xiatao <2761564455@qq.com>
2021-05-08 12:51:37 +0800

    eval metrics in GPU

--- src/eval_metrics.py
+++ src/eval_metrics.py
@@ -1,17 +1,19 @@
 import os
 import numpy as np
-from saliency_metrics import cal_mae, cal_fm, cal_sm, cal_em, cal_wfm, cal_iou
+from saliency_metrics import cal_mae, cal_fm, cal_sm, cal_em, cal_wfm, cal_iou, Eval
 from PIL import Image
 import torchvision.transforms as transforms
 from multiprocessing import Pool, Lock, Manager, Process
 from multiprocessing.managers import BaseManager
 import multiprocessing
+import torch
 
 dataset_path = '..'  ##gt_path
 
 dataset_path_pre = '..'  ##pre_salmap_path
 
 test_datasets = ['test_data']  ##test_datasets_name
+
 
 def parser():
     import argparse
@@ -26,6 +28,7 @@ def parser():
                         )
     args = parser.parse_args()
     return args
+
 
 class eval_dataset:
     def __init__(self, image_root, gt_root):
@@ -61,53 +64,8 @@ class eval_dataset:
             return img.convert('L')
 
 
-def run(index, mae, iou):
-    # with lock:
-    print('predicting for %d / %d' % (index + 1, test_loader.size))
-    sal, gt = test_loader.load_data(index)
-    if sal.size != gt.size:
-        x, y = gt.size
-        sal = sal.resize((x, y))
-    gt = np.asarray(gt, np.float32)
-    gt /= (gt.max() + 1e-8)
-    gt[gt > 0.5] = 1
-    gt[gt != 1] = 0
-    res = sal
-    res = np.array(res)
-    if res.max() == res.min():
-        res = res / 255
-    else:
-        res = (res - res.min()) / (res.max() - res.min())
-    mae.update(res, gt)
-    # sm.update(res, gt)
-    # fm.update(res, gt)
-    # em.update(res, gt)
-    # wfm.update(res, gt)
-    iou.update(res, gt)
-
-
-
-
-class Mymanager(BaseManager):
-    pass
-
-
-def Manager2():
-    m = Mymanager()
-    m.start()
-    return m
-
-
-Mymanager.register('cal_mae', cal_mae)
-Mymanager.register('cal_fm', cal_fm)
-Mymanager.register('cal_sm', cal_sm)
-Mymanager.register('cal_em', cal_em)
-Mymanager.register('cal_wfm', cal_wfm)
-Mymanager.register('cal_iou', cal_iou)
-
 if __name__ == '__main__':
 
-    # lock = Lock()
     args = parser()
     import datetime
 
@@ -118,46 +76,33 @@ if __name__ == '__main__':
         gt_root = dataset_path + '/data/' + dataset + '/mask/'
         test_loader = eval_dataset(sal_root, gt_root)
 
-        # instance share class
-        manager = Manager2()
-        mae = manager.cal_mae()
-        # fm = manager.cal_fm(test_loader.size)
-        # sm = manager.cal_sm()
-        # em = manager.cal_em()
-        # wfm = manager.cal_wfm()
-        iou = manager.cal_iou()
+        metrics = Eval().cuda()
 
-        # mae,fm,sm,em,wfm= cal_mae(),cal_fm(test_loader.size),cal_sm(),cal_em(),cal_wfm()
         f = open(dataset + '-eval.txt', 'w')
 
-        p = Pool(multiprocessing.cpu_count())
+        for index in range(test_loader.size):
+            print('predicting for %d / %d' % (index + 1, test_loader.size))
+            sal, gt = test_loader.load_data(index)
+            if sal.size != gt.size:
+                x, y = gt.size
+                sal = sal.resize((x, y))
+            gt = np.asarray(gt, np.float32)
+            gt /= (gt.max() + 1e-8)
+            gt[gt > 0.5] = 1
+            gt[gt != 1] = 0
+            res = sal
+            res = np.array(res)
+            if res.max() == res.min():
+                res = res / 255
+            else:
+                res = (res - res.min()) / (res.max() - res.min())
 
-        for i in range(test_loader.size):
-            p.apply_async(run, args=(i, mae, iou))
+            res, gt = torch.from_numpy(res).cuda().float(), torch.from_numpy(gt).cuda().float()
+            metrics(res, gt)
 
-        p.close()
-        p.join()
+        MAE, IOU = metrics.show()
 
-        MAE = mae.show()
-        # maxf, meanf, _, _ = fm.show()
-        # sm = sm.show()
-        # em = em.show()
-        # wfm = wfm.show()
-        IOU = iou.show()
-        #
-        # print('dataset: {} MAE: {:.4f} maxF: {:.4f} avgF: {:.4f} wfm: {:.4f} Sm: {:.4f} Em: {:.4f} IOU: {:.4f}'.format(dataset,
-        #                                                                                                    MAE,
-        #                                                                                                    maxf,
-        #                                                                                                    meanf,
-        #                                                                                                    wfm, sm,
-        #                                                                                                    em,IOU))
-        # f.write(
-        #     'dataset: {} MAE: {:.4f} maxF: {:.4f} avgF: {:.4f} wfm: {:.4f} Sm: {:.4f} Em: {:.4f} IOU: {:.4f}'.format(dataset,
-        #                                                                                                  MAE, maxf,
-        #                                                                                                  meanf, wfm,
-        #                                                                                                  sm, em, IOU))
-
-        print('dataset: {} MAE: {:.4f} IOU: {:.4f}'.format(dataset, MAE,IOU))
+        print('dataset: {} MAE: {:.4f} IOU: {:.4f}'.format(dataset, MAE, IOU))
         f.write(
             'dataset: {} MAE: {:.4f} IOU: {:.4f}'.format(dataset, MAE, IOU))
         f.close()
