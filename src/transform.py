af492883dcceb95d61eb34e15dda1dd459e4fecf
xt <276156455@qq.com>
2021-07-11 16:32:46 +0800

    update Resize in transform.py

--- src/transform.py
+++ src/transform.py
@@ -18,7 +18,13 @@ class Resize(object):
         self.W = W
 
     def __call__(self, image):
-        image = cv2.resize(image, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
+        (w, h, c) = image.shape
+        if w >= h:
+            r = 800/image.shape[0]
+            image = cv2.resize(image, dsize=(800, h * r), interpolation=cv2.INTER_LINEAR)
+        elif h > w:
+            r = 800/image.shape[1]
+            image = cv2.resize(image, dsize=(w * r, 800), interpolation=cv2.INTER_LINEAR)
         return image
 
 class ToTensor(object):
