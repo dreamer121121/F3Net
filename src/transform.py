4af4d1a5aa2934673c814fac2e05063263a31360
xt <2761564455@qq.com>
2021-07-12 13:55:32 +0800

    update resize in test to 416*416

--- src/transform.py
+++ src/transform.py
@@ -12,20 +12,16 @@ class Normalize(object):
         image = (image - self.mean) / self.std
         return image
 
+
 class Resize(object):
     def __init__(self, H, W):
         self.H = H
         self.W = W
 
     def __call__(self, image):
-        (w, h, c) = image.shape
-        if w >= h:
-            r = 800/image.shape[0]
-            image = cv2.resize(image, dsize=(800, h * r), interpolation=cv2.INTER_LINEAR)
-        elif h > w:
-            r = 800/image.shape[1]
-            image = cv2.resize(image, dsize=(w * r, 800), interpolation=cv2.INTER_LINEAR)
+        image = cv2.resize(image, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR)
         return image
+
 
 class ToTensor(object):
     def __call__(self, image):
