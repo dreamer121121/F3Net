f9c29587ffa683e3a32e9200fe1d82e5c9fd6a61
xiaotao <2761564455@qq.com>
2021-07-14 11:29:12 +0800

    dd loss

--- src/train.py
+++ src/train.py
@@ -27,6 +27,9 @@ from smoothing import gaussian_blur
 
 import torch.nn.functional as F
 
+
+import utils
+
 log_stream = open('train_L1.log', 'a')
 
 global_step = 0
@@ -123,9 +126,9 @@ def structure_loss(pred, mask, sw=None):
     N, C, W, H = mask.shape
     mc_matrix = np.zeros([N, W, H, C], dtype=np.float)
 
-    L1_loss = 2*F.l1_loss(torch.sigmoid(pred), mask)
+    L1_loss = 2 * F.l1_loss(torch.sigmoid(pred), mask)
 
-    #cal smooth loss
+    # cal smooth loss
     smoothed_mask = gaussian_blur(mask, (9, 9), (2.5, 2.5))
     smooth_loss = F.mse_loss(torch.sigmoid(pred), smoothed_mask)
 
@@ -135,17 +138,12 @@ def structure_loss(pred, mask, sw=None):
         transition = generate_trimap(f_mask_img * 255)
         if transition.sum() == 0:
             transition = np.ones((W, H, C))
-        mc_matrix[i, :, :, :] =  transition[:, :, :]
+        mc_matrix[i, :, :, :] = transition[:, :, :]
 
-    # L = np.ones((N, W, H, C))
     W = im2tensor(mc_matrix).cuda()
     loss_alpha = torch.sqrt(
         torch.square((mask - torch.sigmoid(pred)) * W) + torch.square(torch.Tensor([1e-6]).cuda())).sum(
         dim=(2, 3)) / W.sum(dim=(2, 3))
-
-    #focal loss
-    # focus = FocalLoss()
-    # focal_loss = focus(pred, mask)
 
     if sw:
         sw.add_scalar('alpha_loss', loss_alpha.mean().item(), global_step=global_step)
@@ -162,9 +160,10 @@ def main(Dataset, Network):
     ##parse args
     args = parse_args()
 
-    train_cfg = Dataset.Config(datapath='../data/' + args.dataset, savepath='./out_L1', snapshot=args.resume, mode='train',
+    train_cfg = Dataset.Config(datapath='../data/' + args.dataset, savepath='./out_L1', snapshot=args.resume,
+                               mode='train',
                                batch=args.batch_size,
-                               lr=args.lr, momen=0.9, decay=args.decay, epochs=args.epochs, start=args.start)
+                               lr=args.lr, momen=0.9, decay=args.decay, epochs=args.epochs, start=args.start, self_refine_width1=30, self_refine_width2=15)
 
     eval_cfg = Dataset.Config(datapath='../data/' + args.dataset, mode='test', batch=1, eval_freq=1)
 
@@ -226,7 +225,7 @@ def main(Dataset, Network):
 
             log_stream.write('Valid MAE: {:.4f} Valid IOU: {:.4f} Valid Loss: {:.4f}\n'.format(mae, iou, loss))
             log_stream.flush()
-            sw.add_scalar('eval loss', loss, global_step=epoch+1)
+            sw.add_scalar('eval loss', loss, global_step=epoch + 1)
 
 
 def evaluate(net, loader):
@@ -272,18 +271,14 @@ def evaluate(net, loader):
         Iou = iou.show()
         print('---iou---', Iou)
 
-    return Mae, Iou, Loss/len(loader)
+    return Mae, Iou, Loss / len(loader)
 
 
 def train(net, optimizer, loader, sw, epoch, cfg):
     net.train()
     for step, (image, mask) in enumerate(loader):
         image, mask = image.cuda().float(), mask.cuda().float()
-        # print(image.shape) #(32,3,320,320)
-        # print(mask.shape) #(32,1,320,320)
-        # import sys
-        # sys.exit(0)
-        out1u, out2u, out2r, out3r, out4r, out5r = net(image)
+        out1u, out2u, out2r, out3r, out4r, out5r, pred = net(image)
         loss1u = structure_loss(out1u, mask)
         loss2u = structure_loss(out2u, mask)
 
@@ -292,6 +287,49 @@ def train(net, optimizer, loader, sw, epoch, cfg):
         loss4r = structure_loss(out4r, mask, )
         loss5r = structure_loss(out5r, mask, )
         loss = (loss1u + loss2u) / 2 + loss2r / 2 + loss3r / 4 + loss4r / 8 + loss5r / 16
+
+        alpha_pred_os1, alpha_pred_os4, alpha_pred_os8 = pred['alpha_os1'], pred['alpha_os4'], pred['alpha_os8']
+
+        gauss_filter = torch.tensor([[1., 4., 6., 4., 1.],
+                                     [4., 16., 24., 16., 4.],
+                                     [6., 24., 36., 24., 6.],
+                                     [4., 16., 24., 16., 4.],
+                                     [1., 4., 6., 4., 1.]]).cuda()
+        gauss_filter /= 256.
+        gauss_filter = gauss_filter.repeat(1, 1, 1, 1)
+
+        weight_os8 = torch.ones_like(mask).float()
+        weight_os4 = utils.get_unknown_tensor_from_pred(alpha_pred_os8, rand_width=cfg.self_refine_width1,
+                                                        train_mode=True)  # gl4
+        alpha_pred_os4[weight_os4 == 0] = alpha_pred_os8[weight_os4 == 0]  # 等价于论文中的公式(3)
+        weight_os1 = utils.get_unknown_tensor_from_pred(alpha_pred_os4, rand_width=cfg.self_refine_width2,
+                                                        train_mode=True)
+        alpha_pred_os1[weight_os1 == 0] = alpha_pred_os4[weight_os1 == 0]
+
+        loss_rf = {}
+
+        if cfg.rec_weight > 0:
+            loss_rf['rec'] = (regression_loss(alpha_pred_os1, mask, loss_type='L1',
+                                              weight=weight_os1) * 2 + regression_loss(
+                alpha_pred_os4, mask, loss_type='L1', weight=weight_os4) * 1 + regression_loss(alpha_pred_os8, mask,
+                                                                                               loss_type='L1',
+                                                                                               weight=weight_os8) * 1) / 5.0 * cfg.rec_weight
+
+        if cfg.comp_weight > 0:
+            pass
+
+        if cfg.lap_weight > 0:
+            loss_rf['lap'] = (lap_loss(logit=alpha_pred_os1, target=mask, gauss_filter=gauss_filter,
+                                       loss_type='l1', weight=weight_os1) * 2 + \
+                              lap_loss(logit=alpha_pred_os4, target=mask, gauss_filter=gauss_filter,
+                                       loss_type='l1', weight=weight_os4) * 1 + \
+                              lap_loss(logit=alpha_pred_os8, target=mask, gauss_filter=gauss_filter,
+                                       loss_type='l1',
+                                       weight=weight_os8) * 1) / 5.0 * cfg.lap_weight
+
+        for loss_key in loss_rf.keys():
+            if loss_rf[loss_key] is not None and loss_key in ['rec', 'comp', 'lap']:
+                loss += loss_rf[loss_key]
 
         optimizer.zero_grad()
         loss.backward()
@@ -309,6 +347,87 @@ def train(net, optimizer, loader, sw, epoch, cfg):
                 datetime.datetime.now(), global_step, epoch + 1, cfg.epochs, optimizer.param_groups[0]['lr'],
                 loss.item()))
             log_stream.flush()
+
+
+def regression_loss(logit, target, loss_type='l1', weight=None):
+    """
+    Alpha reconstruction loss
+    :param logit:
+    :param target:
+    :param loss_type: "l1" or "l2"
+    :param weight: tensor with shape [N,1,H,W] weights for each pixel, weight_os1,weigth_os4,...相当于文中的gl !!不要搞混
+    :return:
+    """
+    if weight is None:
+        if loss_type == 'l1':
+            return F.l1_loss(logit, target)
+        elif loss_type == 'l2':
+            return F.mse_loss(logit, target)
+        else:
+            raise NotImplementedError("NotImplemented loss type {}".format(loss_type))
+    else:
+        if loss_type == 'l1':
+            return F.l1_loss(logit * weight, target * weight, reduction='sum') / (torch.sum(weight) + 1e-8)
+        elif loss_type == 'l2':
+            return F.mse_loss(logit * weight, target * weight, reduction='sum') / (torch.sum(weight) + 1e-8)
+        else:
+            raise NotImplementedError("NotImplemented loss type {}".format(loss_type))
+
+
+def lap_loss(logit, target, gauss_filter, loss_type='l1', weight=None):
+    '''
+    Based on FBA Matting implementation:
+    https://gist.github.com/MarcoForte/a07c40a2b721739bb5c5987671aa5270
+    '''
+
+    def conv_gauss(x, kernel):
+        x = F.pad(x, (2, 2, 2, 2), mode='reflect')
+        x = F.conv2d(x, kernel, groups=x.shape[1])
+        return x
+
+    def downsample(x):
+        return x[:, :, ::2, ::2]
+
+    def upsample(x, kernel):
+        N, C, H, W = x.shape
+        cc = torch.cat([x, torch.zeros(N, C, H, W).cuda()], dim=3)
+        cc = cc.view(N, C, H * 2, W)
+        cc = cc.permute(0, 1, 3, 2)
+        cc = torch.cat([cc, torch.zeros(N, C, W, H * 2).cuda()], dim=3)
+        cc = cc.view(N, C, W * 2, H * 2)
+        x_up = cc.permute(0, 1, 3, 2)
+        return conv_gauss(x_up, kernel=4 * gauss_filter)
+
+    def lap_pyramid(x, kernel, max_levels=3):
+        current = x
+        pyr = []
+        for level in range(max_levels):
+            filtered = conv_gauss(current, kernel)
+            down = downsample(filtered)
+            up = upsample(down, kernel)
+            diff = current - up
+            pyr.append(diff)
+            current = down
+        return pyr
+
+    def weight_pyramid(x, max_levels=3):
+        current = x
+        pyr = []
+        for level in range(max_levels):
+            down = downsample(current)
+            pyr.append(current)
+            current = down
+        return pyr
+
+    pyr_logit = lap_pyramid(x=logit, kernel=gauss_filter, max_levels=5)
+    pyr_target = lap_pyramid(x=target, kernel=gauss_filter, max_levels=5)
+    if weight is not None:
+        pyr_weight = weight_pyramid(x=weight, max_levels=5)
+        return sum(regression_loss(A[0], A[1], loss_type=loss_type, weight=A[2]) * (2 ** i) for i, A in
+                   enumerate(zip(pyr_logit, pyr_target, pyr_weight)))
+    else:
+        return sum(regression_loss(A[0], A[1], loss_type=loss_type, weight=None) * (2 ** i) for i, A in
+                   enumerate(zip(pyr_logit, pyr_target)))
 
 
 if __name__ == '__main__':
